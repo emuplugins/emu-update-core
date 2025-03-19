5f6c79a52d07b9321ffc49112a4e432b652898bf
emuplugins <tonnysantanasantos@gmail.com>
2025-03-19 15:52:20 -0300

    changes

--- update-handler.php
+++ update-handler.php
@@ -174,13 +174,7 @@ if (!function_exists('custom_plugin_update_management')) {
 // Function call
 custom_plugin_update_management($self_plugin_dir, $plugin_slug, $desired_plugin_dir);
 
-add_action('admin_init', function() {
-
-	if (isset($_GET['force-check-update'])) {
-
-	delete_transient('json_plugin_info');
-	delete_site_transient('update_plugins');
-
-	}
-
-});
+if (isset($_GET['force-check-update'])) {
+	$transient_key = '_transient_emu_updater_' . $plugin_slug;
+	delete_option($transient_key);	
+}
